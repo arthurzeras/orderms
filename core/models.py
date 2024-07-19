c37d85dd6710a3f8edb2c8d03ea475edace7fe00
Arthur Oliveira <arthurolvmorais@gmail.com>
2024-07-18 23:29:15 -0300

    feat: save received order via queue in database

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    order_code = models.IntegerField(unique=True)
+    customer_code = models.IntegerField()
+
+
+class OrderItem(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
--- core/queue_consumer.py
+++ core/queue_consumer.py
@@ -3,6 +3,8 @@ import logging
 
 import pika
 
+from core.models import Order, OrderItem
+
 QUEUE = "order-created-queue"
 
 
@@ -11,17 +13,34 @@ class OrderCreatedListener:
 
     def __init__(self):
         connection = pika.BlockingConnection(pika.ConnectionParameters(host="broker"))
-
         self.channel = connection.channel()
-
         self.channel.queue_declare(queue=QUEUE)
-
         self.channel.basic_consume(
-            queue=QUEUE, on_message_callback=self.callback, auto_ack=True
+            queue=QUEUE, on_message_callback=self.save_order, auto_ack=True
         )
 
-    def callback(self, channel, method, properties, body):
-        self.logger.info(json.loads(body))
+    def save_order(self, channel, method, properties, body):
+        order_payload = json.loads(body)
+
+        (order, created) = Order.objects.update_or_create(
+            order_code=order_payload["order_code"],
+            defaults={"customer_code": order_payload["customer_code"]},
+        )
+
+        [
+            OrderItem.objects.create(
+                order=order,
+                price=item["price"],
+                product=item["product"],
+                quantity=item["quantity"],
+            )
+            for item in order_payload["items"]
+        ]
+
+        if created:
+            self.logger.info("Order created successfully")
+        else:
+            self.logger.info(f"Order {order.id} updated successfully")
 
     def start(self):
         self.logger.info(f"Listening for messages in queue {QUEUE}")
